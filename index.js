37ea3d2f6e874c2518b432489262b66d30ca0357
kobe koto <91669733+kobe-koto@users.noreply.github.com>
2022-12-12 18:01:33 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -3,8 +3,18 @@
  * Under AGPL 3.0
  */
 
+const Redirect = true; /* 重新导向到目标 */
 const DomainMap = {
-    "koto.eu.org": "koto.cc"
+    "koto.eu.org": "koto.cc",
+    "yallage.koto.eu.org": "yallage.koto.cc",
+    "blog.koto.eu.org": "blog.koto.cc",
+    "file.koto.eu.org": "file.koto.cc",
+    "wwww.koto.eu.org": "koto.cc",
+    "www.koto.eu.org": "koto.cc",
+    "ww.koto.eu.org": "koto.cc",
+    "wwww.koto.cc": "koto.cc",
+    "www.koto.cc": "koto.cc",
+    "ww.koto.cc": "koto.cc"
 }
 
 addEventListener("fetch", event => {
@@ -14,6 +24,12 @@ addEventListener("fetch", event => {
 
 async function SiteMoved (request) {
     let url = new URL(request.url);
+
+    if (DomainMap[url.hostname] && Redirect) {
+        const { pathname, search } = url;
+    
+        return Response.redirect("https://" + DomainMap[url.hostname] + pathname + search, 302)
+    }
 
     let HTML = await GetHTML(!!url.search.match(/debug/i));
 
